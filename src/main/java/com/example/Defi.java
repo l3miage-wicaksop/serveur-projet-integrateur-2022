eeca8733920a03cccc56b244b3f348dad290e3fd
wicaksop <pradityo.wicaksono@etu.univ-grenoble-alpes.fr>
2022-04-26 04:47:48 +0200

    defi model and crud

---
+++ src/main/java/com/example/Defi.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.sql.Timestamp;
+
+public class Defi {
+    public String idDefi;
+    public String titre;
+    public String description;
+    public Timestamp dateCreation;
+}
---
+++ src/main/java/com/example/DefiCRUD.java
@@ -0,0 +1,57 @@
+package com.example;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/defis")
+public class DefiCRUD {
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/")
+    public ArrayList<Defi> allUsers(HttpServletResponse response){
+        try (Connection connection = dataSource.getConnection()) { 
+
+            Statement stmt = connection.createStatement(); 
+            
+            ResultSet rs = stmt.executeQuery("SELECT * FROM defis"); 
+            ArrayList<Defi> L = new ArrayList<Defi>(); 
+           
+            while (rs.next()) { 
+                Defi d = new Defi(); 
+                d.idDefi = rs.getString("idDefi"); 
+                d.titre = rs.getString("titre");
+                d.description = rs.getString("description");
+                d.dateCreation = rs.getTimestamp("dateCreation");
+                
+                L.add(d); 
+            }
+            return L;
+        } catch(Exception e){
+            
+                response.setStatus(500);
+                try{
+                    response.getOutputStream().print(e.getMessage());
+                } catch(Exception e2){
+                    System.err.println(e2.getMessage());
+                }
+                System.err.println(e.getMessage());
+                return null;
+        }   
+    }
+}
